1446cff2742fc6cbc9ce34979a644a7e94c118c5
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-01-31 14:34:43 +0100

    Create init.cpp

---
+++ init.cpp
@@ -0,0 +1,17 @@
+#include <librealsense2/rs.hpp>
+#include <iostream>
+
+int main() {
+    rs2::pipeline p;                 // Top-level API for streaming & processing frames
+    p.start();                       // Configure and start the pipeline
+
+    while (true) {
+        rs2::frameset frames = p.wait_for_frames();        // Block until frames arrive
+        rs2::depth_frame depth = frames.get_depth_frame(); // Get depth frame
+        if (!depth) continue;
+
+        int w = depth.get_width(), h = depth.get_height();
+        float dist = depth.get_distance(w/2, h/2);         // Distance to center pixel
+        std::cout << "The camera is facing an object " << dist << " meters away\r";
+    }
+}
